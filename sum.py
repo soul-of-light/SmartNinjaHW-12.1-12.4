6953cf6cf03b59a5e9b7fbdedabc5b25cda47c2e
Daniel Humer <danielhumer.at@icloud.com>
2020-10-04 22:52:45 +0200

    initial commit

---
+++ sum.py
@@ -0,0 +1,10 @@
+import random
+
+
+def numbers_sum(num1, num2):
+    result = num1 + num2
+    return result
+
+
+print(numbers_sum(random.randint(1, 10), random.randint(1, 10)))
+print(numbers_sum(random.randint(1, 10), random.randint(1, 10)))
